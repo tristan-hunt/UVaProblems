5aeca93ac0a69d3ea9c149e05e55be3f70880a0d
tristan-hunt <thunt@ualberta.ca>
2017-02-21 12:12:37 -0700

    some more work on problems

--- graph_coloring.py
+++ graph_coloring.py
@@ -30,50 +30,80 @@ class Node:
 	def __init__(self, index):
 		self.index = index
 		self.neighbours = set()
+		# self.color = "black"
+		# self.isBlack = True
+		# self.isWhite = False
+
 
 	def add_neighbour(self, neighbour):
 		self.neighbours.add(neighbour)
+		# if (self.isBlack and neighbour.isBlack):
+		# 	return(1)
+		# else:
+		# 	return(0)
 
 	def neighbours(self):
-		return self.neighbours
+		return(self.neighbours)
+
+	# def setBlack(self, color):
+	# 	self.color = "black"
+	# 	self.isBlack = True
+	# 	self.isWhite = False
+
+	# def setWhite(self, color):
+	# 	self.color = "white"
+	# 	self.isBlack = False
+	# 	self.isWhite = True
+
+	def print_neighbours(self):
+		index_str = str(self.index)
+		node_str = ("Index: " + index_str + "\tNeighbours: ") 
+		for neighbour in self.neighbours:
+			node_str = node_str + str(neighbour.index) + " "
+		return(node_str)
 
 	def __repr__(self):
 		return str(self.index)
 
 	def __str__(self):
-		return self.__str__()
+		return self.__repr__()
+
+def add_edge(n1, n2):
+	nodes[n1-1].add_neighbour(nodes[n2-1])
+	nodes[n2-1].add_neighbour(nodes[n1-1])
+
+	if n1 in black and n2 in black:
+		white.add(n1)
+		black.remove(n1)
+
+
 
 num_graphs = int(sys.stdin.readline())
-black = set() # of integers
-white = set() # of integers
-nodes = list() #list of Node Objects
+
 for i in range(0, num_graphs):
+	black = set() # of integers
+	white = set() # of integers
+	nodes = list() #list of Node Objects
+
+
 	info = sys.stdin.readline().split(' ')
 	num_edges = int(info[1])
 	num_nodes = int(info[0])
 
 	for j in range(1, num_nodes+1):
-		nodes.append(Node(j))
+		nodes.append(Node(j)) # NOTE: node 1 has index 0, etc. 
 		black.add(j) 
+
 
 	for j in range(0, num_edges):
 		edge = sys.stdin.readline().split(' ')
 		n1 = int(edge[0])
 		n2 = int(edge[1])
+
+		add_edge(n1, n2)
 		
-		nodes[n1].add_neighbour(nodes[n2])
-		nodes[n2].add_neighbour(nodes[n1])
+	print(len(black))
+	print(" ".join(str(x) for x in black))
 
-
-		if n1 in black and n2 in black:
-			black.remove(n1)
-			white.add(n1)
-
-print(len(black))
-print(" ".join(str(x) for x in black))
-
-for node in nodes:
-	print("Neighbours of node %d", str(node), end = '')
-	for neighbour in node.neighbours():
-		print(neighbour, end = '')
-	print("")
+	for node in nodes:
+		print(node.print_neighbours())
