c18a93b0533fab41923a8512cd2ee5475cace716
tristan-hunt <thunt@ualberta.ca>
2017-02-22 15:15:57 -0700

    automatic answer done

--- division.py
+++ division.py
@@ -11,43 +11,43 @@
 
 import sys
 
-def digits_are_unique(numer, denom, n):
-	if (len(numer) == 4):
-		numer = '0'+numer
-	if (len(denom) == 4):
-		denom = '0'+ denom	
-	
-	string = numer + denom
-
-	if len(set(string)) != len(string):
-		return False
-	else:	
-		msg = numer + " / " + denom + " = " + str(n) +"\n"
-		sys.stdout.write(msg)
-		return True
-
-def print_no_solution(n):
-	message = "There are no solutions for " + str(n) + ".\n"
-	sys.stdout.write(message)
-
 def find_pairs(n):
+	str_numer = ""
+	str_denom = ""
 	solution_found = False
-	for denom in range(1234, 98765):
+	for denom in range(1234, 10000):
+		str_denom = '0' + str(denom)
 		numer = denom*n
 		if numer in range(1234, 98765):
-			if(digits_are_unique(str(numer), str(denom), n)):
+			str_numer = str(numer)
+			if len(str_numer) == 4: str_numer = '0' + str_numer
+
+			if len(set(str_numer + str_denom)) == len(str_numer + str_denom):
+				sys.stdout.write(str_numer + " / " + str_denom + " = " + str(n) +"\n")
 				solution_found = True
+
+
+	for denom in range(10000, 98765):
+		str_denom = str(denom)
+		
+		numer = denom*n
+		if numer in range(1234, 98765):
+			str_numer = str(numer)
+			if len(str_numer) == 4: str_numer = '0' + str_numer
+
+			if len(set(str_numer + str_denom)) == len(str_numer + str_denom):
+				sys.stdout.write(str_numer + " / " + str_denom + " = " + str(n) +"\n")
+				solution_found = True
+	
 	if (solution_found == False):
-		print_no_solution(n)
+		sys.stdout.write("There are no solutions for " + str(n) + ".\n")
+
 
 # Each line of input is a valid integer N
 # Input of 0 terminates the program.
 n = int(next(sys.stdin))
 while (n!= 0):
-	if n == 1:
-		print_no_solution(n)
-	else:
-		pairs = find_pairs(n)
+	pairs = find_pairs(n)
 	sys.stdout.write("\n")
 	n = int(next(sys.stdin))
 
