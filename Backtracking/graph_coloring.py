8f9da2b67d4e637d2a3ae5dc4250110cfb79e50f
tristan-hunt <thunt@ualberta.ca>
2017-03-17 04:19:05 -0600

    more attempts

--- Backtracking/graph_coloring.py
+++ Backtracking/graph_coloring.py
@@ -10,7 +10,8 @@
 #  *
 #  * Solution Summary:
 #  *
-#  *   Sequential Algorithm - runs in O(n) time. 
+#  *   Sequential Algorithm - runs in O(n) time (for finding ANY independent set)
+#  *   Find all independent sets, then choose one of the largest ones. 
 #  *
 #  * Used Resources:
 #  *
@@ -25,7 +26,9 @@
 #  *
 import sys
 from collections import defaultdict
+import itertools
 
+sys.stdin = open('input.txt') # For testing on Windows
 class Graph:
 	def __init__(self, V, E):
 		self.graph = defaultdict(list)
@@ -36,20 +39,57 @@ class Graph:
 		self.graph[u].append(v)
 		self.graph[v].append(u)
 
+	def find_independent_set(self, order):
+		"""
+		Given an order of vertices, find an independent set. 
+		"""
+		l = list() #initialize l to an empty set
+		order = list(order)
+		while (len(order) > 0): # while v is not empty:
+			u = order.pop()
+			l.append(u)
+			for v in self.graph[u]:
+				try:
+					order.remove(v) # remove from list by value
+				except ValueError:
+					pass
+		return l
+
+	#def find_independent_set(self, order):
+
 	def find_max(self):
 		"""
 		Sequential Algorithm as described on wikipedia
 		"""
-		self.l = list() #initialize l to an empty set
 		self.vertices = [v for v in self.graph]
-		while (len(self.vertices) > 0): # while v is not empty:
-			u = self.vertices.pop()
-			self.l.append(u)
-			for v in self.graph[u]:
-				try:
-					self.vertices.remove(u) # remove from list by value
-				except ValueError:
-					pass
+
+		# now find all permutations 
+		max_size = 0
+		max_set = list()
+		for order in itertools.permutations(self.vertices):
+			new_set = self.find_independent_set(order) 
+			if len(max_set) > max_size:
+				print('found solution')
+				max_set = new_set
+				max_size = len(max_set)
+		return (max_set)
+
+def minus(graph, v, flag):
+	if flag: # take away v *and all it's neighbours*
+		for u in graph[v]:
+			graph.pop(u)
+		return graph.pop(v)
+	else: #just return without v
+		return(graph.pop(v))
+
+def maxIndSet(graph):
+	if len(graph) == 0:
+		return 0
+	else:
+		v = next(iter(graph)) #presumably any node in graph
+		with_v = 1 + maxIndSet(minus(graph, v, True))
+		without_v = maxIndSet(minus(graph, v, False))
+		return(max(with_v, without_v))
 
 def load():
 	num_cases = int(sys.stdin.readline())
@@ -71,6 +111,9 @@ def load():
 		next(sys.stdin)
 
 for g in load():
-	g.find_max()
-	for element in g.l:
+
+	max_ind_set = maxIndSet(g.graph)
+	print("order: ")
+	sys.stdout.write("{}\n".format(len(max_ind_set)))
+	for element in max_ind_set:
 		sys.stdout.write("{} ".format(element))
