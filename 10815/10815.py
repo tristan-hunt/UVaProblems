4742ef2a803c43148a8642bfbfb52cfe3cf4f77c
tristan-hunt <thunt@ualberta.ca>
2017-03-30 02:09:57 -0600

    frosh week attempted

--- 10815/10815.py
+++ 10815/10815.py
@@ -1,20 +1,22 @@
 import re
 import string
-from sys import stdin
+import sys
 
 first_dict = set()	
+sys.stdin = open("sample-input.txt") # REMOVE BEFORE SUBMITTING
 
-lines = stdin.read().splitlines()
+lines = sys.stdin.read().splitlines()
 for line in lines:	
 	# Seperate into words
-	words = line.rstrip('\n').split(' ')
+	words = line.strip('\n').split(' ')
 	for word in words:
 		word = re.split('[^a-zA-Z]', word)
 		for token in word:
-			token = token.lower()	
+			token = token.lower()
 			first_dict.add(token)
 	
 andys_dic = sorted(first_dict)
 for word in andys_dic:
-	print(word)
+	if (len(word)!=0):
+		sys.stdout.write("{}\n".format(word))
 
